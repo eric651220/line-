73aff4c4cd0b5cdff88eabf6e6aef79c831354bf
eric651220 <45087348+eric651220@users.noreply.github.com>
2018-11-19 11:11:45 +0800

    Update 20181119.php

--- 20181119.php
+++ 20181119.php
@@ -3,4 +3,4 @@
    $json_obj=json_decode($json_str);// 轉成json格式
  
    $myfile=fopen("log.txt","Wt") or die("unable to open file");//設定一個log.txt來印訊息
-   fwrite(smyfile,"\xEF\XBB\xBF",$json_str);//在字串前面加上\xEF\xBB\xBF轉成utf8格式
+   fwrite(smyfile,"\xEF\xBB\xBF",$json_str);//在字串前面加上\xEF\xBB\xBF轉成utf8格式
