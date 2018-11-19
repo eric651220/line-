ae6bb5aeeeadaa9ebdbbead17e3d7b2b21c4327c
eric651220 <45087348+eric651220@users.noreply.github.com>
2018-11-19 12:17:31 +0800

    Update 20181119.php

--- 20181119.php
+++ 20181119.php
@@ -7,9 +7,10 @@
   
   $sender_userid = $json_obj->events[0]->source->userId; //取得訊息發送者的id
   $sender_txt = $json_obj->events[0]->message->text; //取得訊息內容
+  $sender_replyToken = $json_obj->events[0]->replyToken; //取得訊息的replyToken
   
   $response = array (
-    "to" => $sender_userid,
+    "replyToken" => $sender_replyToken,
     "messages" => array (
       array (
         "type" => "text",
@@ -20,8 +21,8 @@
   
  fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
   $header[] = "Content-Type: application/json";
-  $header[] = "Authorization: Bearer Li9/ePqVK/aOUDxmZXzWJY1x2fKKZjs62TX1/fw6KmI/n6opoMKliLv9UuDDUVVUMmkmR2DM1JjYLUZrQV/E0eebmTSn29ng2xp9bplIOpax92INO3HQoFLPRD8n2CX7CmevgcPzw/UDiWPv09lJTAdB04t89/1O/w1cDnyilFU=";
-  $ch = curl_init("https://api.line.me/v2/bot/message/push");
+  $header[] = "Authorization: Bearer gd1gyH+Pc5TROu9ku5u/5tDvFnffsU8nXU69zXuhTgE0dIS5nVGmx9Js8PwijeUqgFuwWXzyJ14/N5FUmp/UXsmSJbUsxMGA6AW1gozlf6cbEgSGLiC02BEaRa5wUSqE7df8FOANP1WjPW8Mh/TgtwdB04t89/1O/w1cDnyilFU=";
+  $ch = curl_init("https://api.line.me/v2/bot/message/reply");
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
   curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
